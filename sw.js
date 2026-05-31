3e9ad197e2fb02a135e728eb0c988ef52d40e33d
LDinisPT <93349712+LDinisPT@users.noreply.github.com>
2026-05-31 07:05:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'verallia-2026-v7';
+const CACHE = 'verallia-2026-v8';
 const FILES = ['/', '/index.html', '/schedule-data.js', '/manifest.json', '/verallia_logo.avif', '/verallia_logo.avif'];
 
 self.addEventListener('install', e => {
