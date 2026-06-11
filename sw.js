099f3c4449de0b28e168c5bec0743429a0350e11
LDinisPT <93349712+LDinisPT@users.noreply.github.com>
2026-06-11 16:37:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'verallia-2027-v28';
+const CACHE = 'verallia-2027-v29';
 const FILES = ['/', '/index.html', '/schedule-data.js', '/manifest.json', '/verallia_logo.avif'];
 
 // Force update on every install
